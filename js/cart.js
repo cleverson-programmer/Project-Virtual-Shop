44d70c85140edb803ff10e5b158569e8e7c43fea
cleverson-programmer <cleverson.github@gmail.com>
2024-09-24 16:05:32 -0300

    uptadeProductsCart

--- js/cart.js
+++ js/cart.js
@@ -1,4 +1,5 @@
 //Observador para aguardar a requisição e renderização dos cards de renderProcuts.js para depois chamar as funções referentes ao carrinho
+/*
 
 document.addEventListener('DOMContentLoaded', () => {
     const observer = new MutationObserver((mutations) => {
@@ -15,126 +16,82 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 })
 
-if (document.readyState == 'loading') {
-    document.addEventListener('DOMContentLoaded', ready)
-}else {
-    ready()
-}
-
-let totalAmount = "0,00"
-
 function ready() {
   // Botão add produto ao carrinho
   const addToCartButtons = document.getElementsByClassName("addToCart");
   for (var i = 0; i < addToCartButtons.length; i++) {
     addToCartButtons[i].addEventListener("click", addProductToCart);
   }
-
-  //Botão remover produto do carrinho
-  const removeCartProductButtons = document.getElementsByClassName("remove-product-button")
-    for (var i = 0; i < removeCartProductButtons.length; i++) {
-      removeCartProductButtons[i].addEventListener("click", removeProduct)
-    }
-
-    // Mudança valor dos inputs
-  const quantityInputs = document.getElementsByClassName("product-qtd-input")
-    for (var i = 0; i < quantityInputs.length; i++) {
-      quantityInputs[i].addEventListener("change", checkIfInputIsNull)
-    }
-
-  // Botão comprar
-  const purchaseButton = document.getElementsByClassName("purchase-button")[0]
-  purchaseButton.addEventListener("click", makePurchase)
-}
-
-function removeProduct(event) {
-    event.target.parentElement.parentElement.remove()
-    updateTotal()
-}
-
-function checkIfInputIsNull(event) {
-    if (event.target.value === "0") {
-      event.target.parentElement.parentElement.remove()
-    }
-  
-    updateTotal()
 }
 
 function addProductToCart(event){
     const button = event.target
-    const productInfos = button.parentElement
-    const productImage = productInfos.getElementsByClassName("product-image").src
+    console.log(button)
+
+    const cardProduct = button.parentElement.parentElement.parentElement
+    const productImage = cardProduct.getElementsByClassName("product-image")[0].src
     console.log(productImage)
-    const productName = productInfos.getElementsByClassName("product-title").innerText
-    const productPrice = productInfos.getElementsByClassName("product-price").innerText
-  
-    const productsCartNames = document.getElementsByClassName("cart-product-title")
-    for (var i = 0; i < productsCartNames.length; i++) {
-      if (productsCartNames[i].innerText === productName) {
-        productsCartNames[i].parentElement.parentElement.getElementsByClassName("product-qtd-input")[0].value++
-        updateTotal()
-        return
-      }
+    const nameProduct = cardProduct.getElementsByClassName("name-product")[0].innerText
+    //console.log(nameProduct)
+
+    //Recuperando o valor de slice e price do localStorage
+    const sliceProduct = localStorage.getItem('selectedSize')
+    console.log(sliceProduct)
+
+    const selectedPrice = localStorage.getItem('selectedPrice')
+    const priceNumber = parseInt(selectedPrice, 10)
+    if (priceNumber) {
+        console.log(priceNumber);
+        // Aqui você pode usar o valor recuperado conforme necessário
     }
 
-  //Renderizando na tela o card no carrinho
-  let newCartProduct = document.createElement("tr")
-  newCartProduct.classList.add("cart-product")
 
-  newCartProduct.innerHTML =
-    `
-      <td class="product-identification">
-        <img src="${productImage}" alt="${productName}" class="cart-product-image">
-        <strong class="cart-product-title">${productName}</strong>
-      </td>
-      <td>
-        <span class="cart-product-price">${productPrice}</span>
-      </td>
-      <td>
-        <input type="number" value="1" min="0" class="product-qtd-input">
-        <button type="button" class="remove-product-button">Remover</button>
-      </td>
-    `
-
-    const tableBody = document.querySelector(".cart-table tbody")
-    tableBody.append(newCartProduct)
-
-    updateTotal()
-  
-    newCartProduct.getElementsByClassName("remove-product-button")[0].addEventListener("click", removeProduct)
-    newCartProduct.getElementsByClassName("product-qtd-input")[0].addEventListener("change", checkIfInputIsNull)
-}
-
-function makePurchase() {
-    if (totalAmount === "0,00") {
-      alert("Seu carrinho está vazio!")
-    } else {   
-      alert(
-        `
-          Obrigado pela sua compra!
-          Valor do pedido: R$${totalAmount}\n
-          Volte sempre :)
-        `
-      )
-  
-      document.querySelector(".cart-table tbody").innerHTML = ""
-      updateTotal()
+    //Adicionar mais quantidade de itens iguais
+    const productsName = document.querySelector(".name-product")
+    for(var i = 0; i < productsName.length; i++){
+        if(productsName[i].innerText === nameProduct){
+            productsName[i].parentElement.parentElement.getElementsByClassName("amountItems")[0].value++
+        }
     }
-}
 
-// Atualizar o valor total do carrinho
-function updateTotal() {
-    const cartProducts = document.getElementsByClassName("cart-product")
-    totalAmount = 0
-  
-    for (var i = 0; i < cartProducts.length; i++) {
-      const productPrice = cartProducts[i].getElementsByClassName("cart-product-price")[0].innerText.replace("R$", "").replace(",", ".")
-      const productQuantity = cartProducts[i].getElementsByClassName("product-qtd-input")[0].value
-  
-      totalAmount += productPrice * productQuantity
-    }
+    //Renderizando na tela o card no carrinho
+    let newProductCart = document.createElement("div")
+
+    newProductCart.innerHTML = `
+        <div class="flex justify-between mb-2 mt-2 p-2 rounded-md flex-col w-full h-36 bg-white" id="cart-items">
+                <div class="flex flex-row justify-between mt-4">
+                    <div class="text-xl">
+                        <p class="name-product">Sabor: ${nameProduct}</p>
+                        <p id="size-product">Tamanho: ${sliceProduct}</p>
+                    </div>
+                    <div>
+                        <img src="${productImage}" class="w-16 h-16 rounded-full" alt="">
+                    </div>
+                </div>
+                <div class="flex justify-between items-center">
     
-    totalAmount = totalAmount.toFixed(2)
-    totalAmount = totalAmount.replace(".", ",")
-    document.querySelector(".cart-total-container span").innerText = "R$" + totalAmount
+                    <div class="flex gap-3">
+                        <div class=" flex w-4 h-6 border border-solid border-white rounded-md justify-center text-black">
+                            <input type="number" value="1" min="0" class="amountItem border border-solid border-black rounded-md h-10 w-10 flex justify-center text-center">
+                        </div>
+                    </div>
+    
+                    <div class="flex flex-row w-32 justify-between items-center text-xl">
+                        <p class="" id="price-products">${priceNumber}R$</p>
+                        <i class="fa-regular fa-trash-can text-red-600"></i>
+                    </div>
+                </div>
+            </div>
+    `
+    const containerItems = document.querySelector('.containerCartItems')
+    containerItems.appendChild(newProductCart)
+
+    //Função que atualiza o valor total do carrinho
+    uptadeTotal()
+
 }
+
+function uptadeTotal(){
+
+}
+*/
--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 const userIcon = document.getElementById("user-icon");
 const containerAccount = document.getElementById("containerAccount")
 const closeAccount = document.getElementById('close-account')
-const menuCart = document.querySelector("#buy-cart")
 
 const menuContainer = document.getElementById('menuContainer')
 
@@ -10,13 +9,13 @@ const size = document.querySelector(".addSize")
 
 
 const sendOrderWhatsapp = document.querySelector("#btn-send-whatsapp")
-const showCartBottom = document.querySelector("#show-cart")
+
 
 const openingHours = document.querySelector("#opening-hours")
 const arrowDownHeight = document.querySelector("#arrow-down")
 const cardProduct = document.querySelector("#card-product")
 const totalCartValue = document.querySelector("#price-products")
-const closeCartContainer = document.querySelector("#close-cart-button")
+
 const cartCounterItems = document.querySelector("#cart-count")
 
 const nameClient = document.querySelector("#name")
@@ -26,16 +25,6 @@ const addressClientIncorrect = document.querySelector("#address-warn")
 const observationsClient = document.querySelector("#observations")
 
 //Toggle show cart menu
-
-showCartBottom.addEventListener( 'click', ()=> {
-    menuCart.classList.remove('hidden')
-    menuCart.classList.add('flex')
-})
-
-closeCartContainer.addEventListener('click', () => {
-    menuCart.classList.remove('flex')
-    menuCart.classList.add('hidden')
-})
 
 
 // View page account
